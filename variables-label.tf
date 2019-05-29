6be55ce16d87109f131e4005b766cea27e303c27
Steven Nemetz <steven@appzen.com>
2019-05-29 08:48:10 -0700

    Split variable files

---
+++ variables-label.tf
@@ -0,0 +1,42 @@
+//
+// Variables specific to module label
+//
+variable "attributes" {
+  description = "Suffix name with additional attributes (policy, role, etc.)"
+  type        = "list"
+  default     = []
+}
+
+variable "delimiter" {
+  description = "Delimiter to be used between `name`, `namespaces`, `attributes`, etc."
+  default     = "-"
+}
+
+variable "environment" {
+  description = "Environment (ex: `dev`, `qa`, `stage`, `prod`). (Second or top level namespace. Depending on namespacing options)"
+}
+
+variable "name" {
+  description = "Base name for resources"
+}
+
+variable "namespace-env" {
+  description = "Prefix name with the environment. If true, format is: <env>-<name>"
+  default     = true
+}
+
+variable "namespace-org" {
+  description = "Prefix name with the organization. If true, format is: <org>-<env namespaced name>. If both env and org namespaces are used, format will be <org>-<env>-<name>"
+  default     = false
+}
+
+variable "organization" {
+  description = "Organization name (Top level namespace)."
+  default     = ""
+}
+
+variable "tags" {
+  description = "A map of additional tags"
+  type        = "map"
+  default     = {}
+}
