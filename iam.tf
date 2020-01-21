40a5f333823bd7f014ca7da67ae42c2fd7f6db65
wesleung <wes@appzen.com>
2020-01-20 22:06:09 -0800

    truncate aws_iam_role name_prefix to 32 char

--- iam.tf
+++ iam.tf
@@ -63,7 +63,7 @@ data "aws_iam_policy_document" "assume_role_service" {
 # var.task_role_arn == ""
 resource "aws_iam_role" "task" {
   count              = "${module.enabled.value}"
-  name_prefix        = "${var.service_identifier}-${var.task_identifier}-ecsTaskRole"
+  name_prefix        = "${substr("${var.service_identifier}-${var.task_identifier}-ecsTaskRole", 0, min(length("${var.service_identifier}-${var.task_identifier}-ecsTaskRole"), 32))}"
   path               = "/${var.service_identifier}/"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_task.json}"
 }
@@ -78,7 +78,7 @@ resource "aws_iam_role_policy" "task" {
 
 resource "aws_iam_role" "service" {
   count              = "${module.enabled.value}"
-  name_prefix        = "${var.service_identifier}-${var.task_identifier}-ecsServiceRole"
+  name_prefix        = "${substr("${var.service_identifier}-${var.task_identifier}-ecsServiceRole", 0, min(length("${var.service_identifier}-${var.task_identifier}-ecsServiceRole"), 32))}"
   path               = "/${var.service_identifier}/"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_service.json}"
 }
